5517c6c4f8604626ea6c38188d8402e3b3a91c71
Wulanpuspita <wulanpuspitasari3023@gmail.com>
2019-09-13 17:09:46 +0800

    Tugas Laravel 2

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -26,6 +26,20 @@
                         </div>
 
                         <div class="form-group row">
+                            <label for="phone" class="col-md-4 col-form-label text-md-right">{{ __('Phone Number') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="phone" type="text" class="form-control @error('phone') is-invalid @enderror" name="phone" value="{{ old('phone') }}" required autocomplete="phone" autofocus>
+
+                                @error('phone')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
@@ -61,6 +75,33 @@
                             </div>
                         </div>
 
+                        <div class="col-md-6">
+
+                          <div class="captcha">
+
+                          <span>{!! captcha_img() !!}</span>
+
+                          <button type="button" class="btn btn-success btn-refresh"><i class="fa fa-refresh"></i></button>
+
+                          </div>
+
+                          <input id="captcha" type="text" class="form-control" placeholder="Enter Captcha" name="captcha">
+
+
+                          @if ($errors->has('captcha'))
+
+                              <span class="help-block">
+
+                                  <strong>{{ $errors->first('captcha') }}</strong>
+
+                              </span>
+
+                          @endif
+
+                      </div>
+
+                  </div>
+                  
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
