f47ce056dc2cf91eafa5a0e331ea85f54209757d
Wulanpuspita <wulanpuspitasari3023@gmail.com>
2019-09-26 11:39:44 +0800

    Tugas Laravel 4

--- resources/views/pengumuman/index.blade.php
+++ resources/views/pengumuman/index.blade.php
@@ -1,29 +1,32 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title> pengumuman </title>
-</head>
-<body>
-	<table border="1">
-		<tr>
-			<td>ID</td>
-			<td>Nama</td>
-			<td>Judul</td>
-			<td>Isi</td>
-			<td>User ID</td>
-		</tr>
-        @foreach($pengumuman as $item)
+@extends('layouts.app')
 
-        <tr>
-        	<td>{!! $item->id !!}</td>
-        	<td>{!! $item->nama !!}</td>
-        	<td>{!! $item->judul !!}</td>
-        	<td>{!! $item->isi !!}</td>
-        	<td>{!! $item->users_id !!}</td>
-        </tr>
-        @endforeach
+@section('content')
 
-	</table>
-
-</body>
-</html>
+</br></br></br></br>
+    
+<div class="container" style="margin-top:40px">
+    <div class="row">
+        <div class="col-md-9">
+              <div class="panel panel-default">
+               <div class="panel-body">
+                <div class="post-detail">
+                  @foreach($pengumuman as $item)
+                  <h3>{!! $item->judul !!}</h3>
+                  
+                    <div class="info-meta">
+                        <ul class="list-inline">
+                            <li><i class="fa fa-clock-o"></i> {!! $item->created_at!!}</li>
+                            <li><i class="fa fa-user"></i> {!! $item->users_id !!}</li>
+                            <li class="pull-right">{!! $item->kategori_pengumuman_id !!}</li>
+                        </ul>
+                    </div>
+                    <div class="info-meta">{!! $item->isi !!}</div>
+                    @endforeach
+                  </div>
+               </div>
+            </div>
+         </div>   
+</div>
+</div>  
+</div>   
+@endsection
